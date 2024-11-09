3a7511da66194cf89d60d83494cb03a9540bf065
Ivania10-10Ruz <ivaniaruz30@gmail.com>
2024-11-08 22:04:19 -0600

    Add files via upload

---
+++ Libro.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Graciela
+ */
+public class Libro {
+    String titulo;
+
+    // Constructor
+    public Libro(String titulo) {
+        this.titulo = titulo;
+    }
+
+    // Método
+    public void mostrarTitulo() {
+        System.out.println("El título del libro es: " + titulo);
+    }
+}
+
+
---
+++ Tarea.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tarea;
+
+/**
+ *
+ * @author Graciela
+ */
+public class Tarea {
+
+    public static void main(String[] args) {
+        System.out.println("Holis, mi nombre es Ivania!");
+    }
+}
